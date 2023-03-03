730a39444d69a141ae89d7634fbfda0264a63b17
Dlorodev <dlorodev@gmail.com>
2023-03-03 11:59:50 -0500

    finishing regular expressions course

--- javascript-algorithms-and-data-structures/regular-expressions.js
+++ javascript-algorithms-and-data-structures/regular-expressions.js
@@ -63,4 +63,20 @@ To create a negated character set, you place a caret character (^) after the ope
 let quoteSample4 = "3 blind mice.";
 let negativeRegex = /[^3aeiou]/ig;
 let resultNegative = quoteSample4.match(negativeRegex);
-console.log(resultNegative);
+//console.log(resultNegative);
+
+//PRACTICE
+/* Restrict Possible Usernames
+Usernames are used everywhere on the internet. They are what give users a unique identity on their favorite sites.
+You need to check all the usernames in a database. Here are some simple rules that users have to follow when creating their username.
+Usernames can only use alpha-numeric characters.
+The only numbers in the username have to be at the end. There can be zero or more of them at the end. Username cannot start with the number.
+Username letters can be lowercase and uppercase.
+Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters.
+Change the regex userCheck to fit the constraints listed above. */
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/gi; // Change this line
+let usernameTest = userCheck.test(username);
+let usernameResult = username.match(userCheck);
+//console.log(usernameTest);
+//console.log(usernameResult);
